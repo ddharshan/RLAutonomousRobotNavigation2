fd9ff736eb95afd45b06631bf24582083ea5336f
Dharshan <108181384+ddharshan@users.noreply.github.com>
2022-10-26 10:16:05 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='AutonomousRobotNavigation2',    #change----------------------------------
+     version='1',
+     packages=['AutonomousRobotNavigation2'],  #change---------------------------------
+     install_requires=['gym', 'numpy'],
+)
