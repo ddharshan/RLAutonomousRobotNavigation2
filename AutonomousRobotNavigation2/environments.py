33bdbbd0648ba768cdd71fdd66e7ab0129b1a001
Dharshan <108181384+ddharshan@users.noreply.github.com>
2022-11-01 18:57:33 +0530

    Update environments.py

--- AutonomousRobotNavigation2/environments.py
+++ AutonomousRobotNavigation2/environments.py
@@ -62,7 +62,7 @@ class BaseEnv(gym.Env):
             max_turn: float = np.pi/9,
             max_acceleration: float = 1,
             delta_t: float = 0.005,
-            max_step: int = 400,
+            max_step: int = 500,
             penalty: float = 0.001,
             break_value: float = 0.1,
     ):
@@ -600,7 +600,7 @@ class MovingEnv(BaseEnv):
             max_turn: float = np.pi/9,
             max_acceleration: float = 1,
             delta_t: float = 0.005,
-            max_step: int = 400,
+            max_step: int = 500,
             penalty: float = 0.001,
             break_value: float = 0.1,
     ):
