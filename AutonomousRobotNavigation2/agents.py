91c86185e6cb3f138b5b0d6bcc9a8d31471addac
Dharshan <108181384+ddharshan@users.noreply.github.com>
2022-12-10 07:50:17 +0530

    Update agents.py

--- AutonomousRobotNavigation2/agents.py
+++ AutonomousRobotNavigation2/agents.py
@@ -22,24 +22,6 @@ class BaseAgent:
         self.p2y = None
         self.p2theta = None  # direction of pedestrian 2
         self.p2speed = None
-        
-        #pedestrian 3
-        self.p3x = None
-        self.p3y = None
-        self.p3theta = None  # direction of pedestrian 3
-        self.p3speed = None
-
-        #pedestrian 4
-        self.p4x = None
-        self.p4y = None
-        self.p4theta = None  # direction of pedestrian 4
-        self.p4speed = None
-        
-        #pedestrian 5
-        self.p5x = None
-        self.p5y = None
-        self.p5theta = None  # direction of pedestrian 5
-        self.p5speed = None
 
     def accelerate(self, value: float) -> None:
         raise NotImplementedError
@@ -64,31 +46,9 @@ class BaseAgent:
 
     def p2turn(self, value: float) -> None:
         raise NotImplementedError
-    
-#pedestrian 3
-    def p3accelerate(self, value: float) -> None:
-        raise NotImplementedError
+     
 
-    def p3turn(self, value: float) -> None:
-        raise NotImplementedError
-        
-        
-#pedestrian 4
-    def p4accelerate(self, value: float) -> None:
-        raise NotImplementedError
-
-    def p4turn(self, value: float) -> None:
-        raise NotImplementedError
-
-      
-#pedestrian 5
-    def p5accelerate(self, value: float) -> None:
-        raise NotImplementedError
-
-    def p5turn(self, value: float) -> None:
-        raise NotImplementedError
-        
-    def reset(self, x: float, y: float, direction: float, p1x: float, p1y: float, p1direction: float, p2x: float, p2y: float, p2direction: float, p3x: float, p3y: float, p3direction: float, p4x: float, p4y: float, p4direction: float,  p5x: float, p5y: float, p5direction: float) -> None:
+    def reset(self, x: float, y: float, direction: float, p1x: float, p1y: float, p1direction: float, p2x: float, p2y: float, p2direction: float) -> None:
         self.x = x
         self.y = y
         self.speed = 0
@@ -103,25 +63,7 @@ class BaseAgent:
         self.p2y = p2y
         self.p2speed = 0
         self.p2theta = p2direction
-        
-#pedestrian 3
-        self.p3x = p3x
-        self.p3y = p3y
-        self.p3speed = 0
-        self.p3theta = p3direction
 
-#pedestrian 4
-        self.p4x = p4x
-        self.p4y = p4y
-        self.p4speed = 0
-        self.p4theta = p4direction
-        
-#pedestrian 5
-        self.p5x = p5x
-        self.p5y = p5y
-        self.p5speed = 0
-        self.p5theta = p5direction
-        
     def _step(self) -> None:
         angle = self.theta if self.phi is None else self.phi
         self.x += self.delta_t * self.speed * np.cos(angle)
@@ -134,21 +76,6 @@ class BaseAgent:
         p2angle = self.p2theta 
         self.p2x += self.delta_t * self.p2speed * np.cos(p2angle)
         self.p2y += self.delta_t * self.p2speed * np.sin(p2angle)
-#pedestrian 3
-        p3angle = self.p3theta 
-        self.p3x += self.delta_t * self.p3speed * np.cos(p3angle)
-        self.p3y += self.delta_t * self.p3speed * np.sin(p3angle)
-#pedestrian 4
-        p4angle = self.p4theta 
-        self.p4x += self.delta_t * self.p4speed * np.cos(p4angle)
-        self.p4y += self.delta_t * self.p4speed * np.sin(p4angle)
-#pedestrian 5
-        p5angle = self.p5theta 
-        self.p5x += self.delta_t * self.p5speed * np.cos(p5angle)
-        self.p5y += self.delta_t * self.p5speed * np.sin(p5angle)
-
-
-
 
 
 
@@ -188,37 +115,4 @@ class MovingAgent(BaseAgent):
     def p2turn(self, value: float) -> None:
         self.p2theta = (self.p2theta + value) % (2 * np.pi)
         self._step()
-
-#Pedestrian 3
-
-    def p3accelerate(self, value: float) -> None:
-        self.p3speed += value
-        self._step()
-
-    def p3turn(self, value: float) -> None:
-        self.p3theta = (self.p3theta + value) % (2 * np.pi)
-        self._step()
-        
-        
-#Pedestrian 4
-
-    def p4accelerate(self, value: float) -> None:
-        self.p4speed += value
-        self._step()
-
-    def p4turn(self, value: float) -> None:
-        self.p4theta = (self.p4theta + value) % (2 * np.pi)
-        self._step()
-        
-#Pedestrian 5
-
-    def p5accelerate(self, value: float) -> None:
-        self.p5speed += value
-        self._step()
-
-    def p5turn(self, value: float) -> None:
-        self.p5theta = (self.p5theta + value) % (2 * np.pi)
-        self._step()
- 
-
  
