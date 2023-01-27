c8eab76b2c4a1103abdba83684fee9d7a0ca8b54
Dharshan <108181384+ddharshan@users.noreply.github.com>
2023-01-27 10:12:09 +0530

    Update agents.py

--- AutonomousRobotNavigation2/agents.py
+++ AutonomousRobotNavigation2/agents.py
@@ -16,6 +16,12 @@ class BaseAgent:
         self.p1y = None
         self.p1theta = None  # direction of the agent
         self.p1speed = None
+        
+        #pedestrian 2
+        self.p2x = None
+        self.p2y = None
+        self.p2theta = None  # direction of pedestrian 2
+        self.p2speed = None
 
     def accelerate(self, value: float) -> None:
         raise NotImplementedError
@@ -33,7 +39,16 @@ class BaseAgent:
     def p1turn(self, value: float) -> None:
         raise NotImplementedError
         
-    def reset(self, x: float, y: float, direction: float, p1x: float, p1y: float, p1direction: float) -> None:
+        
+#pedestrian 2
+    def p2accelerate(self, value: float) -> None:
+        raise NotImplementedError
+
+    def p2turn(self, value: float) -> None:
+        raise NotImplementedError
+     
+
+    def reset(self, x: float, y: float, direction: float, p1x: float, p1y: float, p1direction: float, p2x: float, p2y: float, p2direction: float) -> None:
         self.x = x
         self.y = y
         self.speed = 0
@@ -43,6 +58,11 @@ class BaseAgent:
         self.p1y = p1y
         self.p1speed = 0
         self.p1theta = p1direction
+#pedestrian 2
+        self.p2x = p2x
+        self.p2y = p2y
+        self.p2speed = 0
+        self.p2theta = p2direction
 
     def _step(self) -> None:
         angle = self.theta if self.phi is None else self.phi
@@ -52,6 +72,12 @@ class BaseAgent:
         p1angle = self.p1theta 
         self.p1x += self.delta_t * self.p1speed * np.cos(p1angle)
         self.p1y += self.delta_t * self.p1speed * np.sin(p1angle)
+#pedestrian 2
+        p2angle = self.p2theta 
+        self.p2x += self.delta_t * self.p2speed * np.cos(p2angle)
+        self.p2y += self.delta_t * self.p2speed * np.sin(p2angle)
+
+
 
 class MovingAgent(BaseAgent):
     def __init__(self, break_value: float, delta_t: float):
@@ -80,3 +106,13 @@ class MovingAgent(BaseAgent):
         self.p1theta = (self.p1theta + value) % (2 * np.pi)
         self._step()
         
+#Pedestrian 2
+
+    def p2accelerate(self, value: float) -> None:
+        self.p2speed += value
+        self._step()
+
+    def p2turn(self, value: float) -> None:
+        self.p2theta = (self.p2theta + value) % (2 * np.pi)
+        self._step()
+ 
