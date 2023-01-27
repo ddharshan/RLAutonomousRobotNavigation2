ed02e858e054ce8deab1a2a5766ed0646cde3c97
Dharshan <108181384+ddharshan@users.noreply.github.com>
2023-01-27 13:26:29 +0530

    Update environments.py

--- AutonomousRobotNavigation2/environments.py
+++ AutonomousRobotNavigation2/environments.py
@@ -117,12 +117,12 @@ class BaseEnv(gym.Env):
         #self.agent.reset(-0.7, 0.6, np.pi, 0, 1, 1.5*(np.pi))  #Initiallizing the robot
         
         #Randomizing the initial position of pedestrian1 which will ensure the dynmaic environment(Rather than fixing the starting position of pedestrian, randomizing will be more effective. Because the dynamic is high)   
-        low = [-0.7,-0.7, 0,0, 0, 0]   #limiting the starting of randomization of pedestrian 1 greater than -0.3 in x plane which will ensure not collide with the robot at starting 
-        high = [0.7,0.7,0, 0, 0, 0]  
+        low = [-1,-1, 0,-1, -1, 0]   #limiting the starting of randomization of pedestrian 1 greater than -0.3 in x plane which will ensure not collide with the robot at starting 
+        high = [1,1,2*(np.pi), 1, 1, 2*(np.pi)]  
         
         #self.pedestrian1.reset(*self.np_random.uniform(low, high)) #randomizing the starting of pedestrian 1
         self.agent.reset(*self.np_random.uniform(low, high))  #Initiallizing the robot
-        self.pedestrian1.reset(0,0,0,-1, 0, 0) #randomizing the starting of pedestrian 1
+        self.pedestrian1.reset(*self.np_random.uniform(low, high)) #randomizing the starting of pedestrian 1
         
  
         limit = self.field_size-self.target_radius
